4e3c1afd4be99b5c49b9931d87a5e29c162d9ee7
glennpacker <glenn.packer@branthill.co.uk>
2020-12-15 15:20:06 +0000

    composition vue 3 initial

---
+++ src/components/CompositionModular.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <p>Capacity: {{ capacity }} ({{ spacesLeft }} spaces)</p>
+        <button @click="increaseCapacity()">Increase Capacity</button>
+        <h2>Attending</h2>
+        <ul>
+        <li v-for="(name, index) in attending" :key="index">
+            {{ name }}
+        </li>
+        </ul>
+  </div>
+</template>
+<script>
+    import { ref, computed } from "vue";
+    export default {
+        setup() {
+            return useEventSpace();
+        }
+    };
+
+    function useEventSpace() {
+        const capacity = ref(4);
+        const attending = ref(["Tim", "Bob", "Joe"]);
+        const spacesLeft = computed(() => {
+            return capacity.value - attending.value.length;
+        });
+        function increaseCapacity() {
+            capacity.value++;
+        }
+        return { capacity, attending, spacesLeft, increaseCapacity };
+    }
+</script>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <composition-refs />
+    <reactive-composition />
+    <composition-modular />
+    <composition-first />
+    <composition-second />
+  </div>
+</template>
+
+<script>
+import CompositionFirst from './CompositionFirst';
+import CompositionModular from './CompositionModular';
+import CompositionRefs from './CompositionRefs';
+import ReactiveComposition from './ReactiveComposition';
+
+import CompositionSecond from './CompositionSecond';
+
+export default {
+  name: 'HelloWorld',
+  components: {
+      CompositionRefs,
+      CompositionFirst,
+      CompositionModular,
+      CompositionSecond,
+      ReactiveComposition
+  },
+  props: {
+    msg: String
+  }
+}
+</script>
